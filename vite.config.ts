a40c067d368945f67a27916026756a04eb52dbb9
=Nguyen Khac Hieu <hieunk12@vingroup.net>
2026-01-21 14:04:47 +0700

    update-1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/eng-quiz/', // 🔥 đúng bằng tên repo
+});
